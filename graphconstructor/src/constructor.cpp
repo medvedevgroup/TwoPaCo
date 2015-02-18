bc5079b87f071c32818342bc0ba6539a9429885d
ilyaminkin <ivminkin@gmail.com>
2015-02-18 14:46:47 -0500

    Added conditional counting of all vertices

--- graphconstructor/src/constructor.cpp
+++ graphconstructor/src/constructor.cpp
@@ -16,26 +16,27 @@
 #include "vertexenumerator.h"
 
 int main(int argc, char * argv[])
-
 {
 	assert(Sibelia::Runtests());
 	
 	try
 	{		
-		std::vector<std::string> fileName(argv + 2, argv + argc);
+		std::vector<std::string> fileName(argv + 3, argv + argc);
 		std::stringstream ss(*(argv + 1));
 		size_t filterSize;
 		ss >> filterSize;
 		Sibelia::VertexEnumerator vid(fileName, 25, filterSize);
-
-		std::vector<std::string> all;
-		vid.Dump(std::back_inserter(all));
-		std::vector<std::string> rev;
-		std::transform(all.begin(), all.end(), std::back_inserter(rev), static_cast<std::string(*)(const std::string&)>(Sibelia::DnaString::RevComp));
-		all.insert(all.begin(), rev.begin(), rev.end());
-		std::sort(all.begin(), all.end());
-		all.erase(std::unique(all.begin(), all.end()), all.end());
-		std::cout << "All = " << all.size();
+		if (std::string(*(argv + 2)) == "1")
+		{
+			std::vector<std::string> all;
+			vid.Dump(std::back_inserter(all));
+			std::vector<std::string> rev;
+			std::transform(all.begin(), all.end(), std::back_inserter(rev), static_cast<std::string(*)(const std::string&)>(Sibelia::DnaString::RevComp));
+			all.insert(all.begin(), rev.begin(), rev.end());
+			std::sort(all.begin(), all.end());
+			all.erase(std::unique(all.begin(), all.end()), all.end());
+			std::cout << "All = " << all.size();
+		}		
 	}
 	catch (const std::runtime_error & msg)
 	{
