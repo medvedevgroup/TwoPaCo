f62d523f3238dd0c5914cd5bcbb3bca130ea4341
ilyaminkin <ivminkin@gmail.com>
2015-07-08 19:50:42 -0400

    A rework of code

--- graphconstructor/src/dnastring.cpp
+++ graphconstructor/src/dnastring.cpp
@@ -2,27 +2,25 @@
 #include <algorithm>
 
 #include "DnaString.h"
-#include "lib/SpookyV2.h"
 
 namespace Sibelia
 {
-	const std::string DnaPiece::LITERAL = "ACGT";
-	const std::string DnaString::LITERAL = "ACGT";	
-
-	DnaPiece::DnaPiece() :  body_(0)
-	{
-	}
-
-	DnaPiece::DnaPiece(uint64_t size, uint64_t body) :  body_(body)
-	{
-		if (size != sizeof(body) * 4)
-		{
-			uint64_t mask = (uint64_t(1) << (size * 2));
-			body_ &= (mask - 1);
-		}
-	}
-
-	uint64_t DnaPiece::MakeUp(char ch)
+	const std::string DnaString::LITERAL = "ACGT";
+//
+//	DnaPiece::DnaPiece() : body_(0)
+//	{
+//	}
+//
+//	DnaPiece::DnaPiece(uint64_t size, uint64_t body) : body_(body)
+//	{
+//		if (size != sizeof(body) * 4)
+//		{
+//			uint64_t mask = (uint64_t(1) << (size * 2));
+//			body_ &= (mask - 1);
+//		}
+//	}
+//
+	uint64_t DnaString::MakeUp(char ch)
 	{
 		switch (ch)
 		{
@@ -33,81 +31,55 @@ namespace Sibelia
 		case 'G':
 			return 2;
 		case 'T':
-			return 3;		
+			return 3;
 		}
 
 		return 0;
 	}
+//
+//	char DnaPiece::PopFront()
+//	{
+//		char ret = GetChar(0);
+//		body_ &= ~0x3;
+//		body_ >>= 2;
+//		return ret;
+//	}
+//
+//	void DnaPiece::AppendFront(char ch)
+//	{
+//		body_ <<= 2;
+//		body_ |= DnaString::MakeUp(ch);
+//	}
+//
+//	uint64_t DnaPiece::GetBody() const
+//	{
+//		return body_;
+//	}
+//
+//
 
-	char DnaPiece::PopFront()
-	{	
-		char ret = GetChar(0);
-		body_ &= ~0x3;
-		body_ >>= 2;
-		return ret;
-	}
-
-	void DnaPiece::AppendFront(char ch)
-	{
-		body_ <<= 2;
-		body_ |= MakeUp(ch);
-	}
-
-	uint64_t DnaPiece::GetBody() const
-	{
-		return body_;
-	}
-
-	uint64_t DnaPiece::Prefix(uint64_t body, uint64_t size)
-	{
-		uint64_t mask = (uint64_t(1) << (size * 2));
-		return body & (mask - 1);
-	}
-
-	char DnaPiece::GetChar(uint64 idx) const
-	{
-		uint64_t charIdx = body_ >> (2 * idx);
-		return LITERAL[charIdx & 0x3];
-	}
-
-	void DnaPiece::SetChar(uint64_t idx, char ch)
-	{
-		uint64_t mask = uint64_t(0x3) << (idx * 2);
-		body_ &= ~(mask);
-		body_ |= MakeUp(ch) << (2 * idx++);
-	}
-
-	std::string DnaPiece::ToString(size_t size_) const
-	{
-		std::string ret(size_, ' ');
-		for (size_t i = 0; i < size_; i++)
-		{
-			ret[i] = GetChar(i);
-		}
-
-		return ret;
-	}
+//
 	
+//
+//
 
-	DnaPiece::DnaPiece(std::string::const_iterator begin, std::string::const_iterator end) : body_(0)
+//
+//	bool operator == (const DnaPiece & a, const DnaPiece & b)
+//	{
+//		return a.GetBody() == b.GetBody();
+//	}
+//
+//	bool operator != (const DnaPiece & a, const DnaPiece & b)
+//	{
+//		return !(a == b);
+//	}
+//
+//	//-------------------------------------------------------------------------
+
+	void DnaString::Clear()
 	{
-		for (size_t size = 0; begin != end; size++, --begin)
-		{
-			SetChar(size, *begin);
-		}
+		size_ = 0;
 	}
-
-	bool operator == (const DnaPiece & a, const DnaPiece & b)
-	{
-		return a.GetBody() == b.GetBody();
-	}
-
-	bool operator != (const DnaPiece & a, const DnaPiece & b)
-	{
-		return !(a == b);
-	}
-
-	//-------------------------------------------------------------------------
 
 	DnaString::~DnaString()
 	{
@@ -119,32 +91,42 @@ namespace Sibelia
 		std::swap(str_, str.str_);
 	}
 
-	DnaString::DnaString(size_t size, bool capacity) : size_(capacity ? 0 : size), capacity_(CalculateCapacity(size)),
-		str_(new DnaPiece[capacity_])
+	DnaString::DnaString(size_t maxSize, size_t size) : size_(size), capacity_(CalculateCapacity(maxSize)),
+		str_(new uint64_t[capacity_])
 	{
 
 	}
 
-	void DnaString::GetCoord(uint64_t idx, uint64_t & element, uint64_t & pos) const
+	uint64_t DnaString::TranslateIdx(uint64_t & idx) const
 	{
-		pos = idx & ((uint64_t(1) << uint64_t(5)) - 1);
-		element = idx >> 5;
+		uint64_t ret = idx >> 5;
+		idx = idx & ((uint64_t(1) << uint64_t(5)) - 1);
+		return ret;
 	}
 
 	char DnaString::GetChar(uint64_t idx) const
-	{
-		uint64_t pos;
-		uint64_t element;
-		GetCoord(idx, element, pos);		
-		return str_[element].GetChar(pos);
+	{		
+		uint64_t element = TranslateIdx(idx);
+		return GetChar(element, idx);
 	}
 
 	void DnaString::SetChar(uint64_t idx, char ch)
 	{
-		uint64_t pos;
-		uint64_t element;
-		GetCoord(idx, element, pos);
-		str_[element].SetChar(pos, ch);
+		uint64_t element = TranslateIdx(idx);
+		SetChar(element, idx, ch);
+	}
+
+	char DnaString::GetChar(uint64_t element, uint64_t idx) const
+	{
+		uint64_t charIdx = str_[element] >> (2 * idx);
+		return DnaString::LITERAL[charIdx & 0x3];
+	}
+
+	void DnaString::SetChar(uint64_t element, uint64_t idx, char ch)
+	{
+		uint64_t mask = uint64_t(0x3) << (idx * 2);
+		str_[element] &= ~(mask);
+		str_[element] |= DnaString::MakeUp(ch) << (2 * idx++);
 	}
 
 	size_t DnaString::GetSize() const
@@ -154,7 +136,7 @@ namespace Sibelia
 
 	size_t DnaString::MaxSize() const
 	{
-		return capacity_ * DnaPiece::CAPACITY;
+		return capacity_ * UNIT_CAPACITY;
 	}
 	
 	uint64_t* DnaString::GetBody() const
@@ -162,15 +144,11 @@ namespace Sibelia
 		return reinterpret_cast<uint64_t*>(str_);
 	}
 
-
 	DnaString::DnaString(const std::string & str) :size_(str.size()), capacity_(CalculateCapacity(str.size()))
 	{
-		size_t remain = size_;
-		for (size_t pos = 0; remain > 0; ++pos)
+		for (size_t i = 0; i < str.size(); i++)
 		{
-			size_t current = std::min(remain, DnaPiece::CAPACITY);
-			str_[pos] = DnaPiece(str.begin() + pos * DnaPiece::CAPACITY, str.begin() + pos * DnaPiece::CAPACITY + remain);
-			remain -= current;
+			SetChar(i, str[i]);
 		}
 	}
 	
@@ -181,16 +159,13 @@ namespace Sibelia
 
 	std::string DnaString::ToString() const
 	{
-		std::stringstream ss;
-		size_t remain = size_;
-		for (size_t pos = 0; remain > 0; ++pos)
+		std::string ret(size_, ' ');
+		for (size_t i = 0; i < size_; i++)
 		{
-			size_t current = std::min(remain, DnaPiece::CAPACITY);
-			ss << str_[pos].ToString(current);
-			remain -= current;
+			ret[i] = GetChar(i);
 		}
 
-		return ss.str();
+		return ret;
 	}
 
 	std::string DnaString::RevComp(const std::string & str)
@@ -202,8 +177,8 @@ namespace Sibelia
 
 	size_t DnaString::CalculateCapacity(size_t size)
 	{
-		size_t mainPart = size / DnaPiece::CAPACITY;
-		return mainPart + (mainPart * DnaPiece::CAPACITY == size ? 0 : 1);
+		size_t mainPart = size / UNIT_CAPACITY;
+		return mainPart + (mainPart * UNIT_CAPACITY == size ? 0 : 1);
 	}
 
 	char DnaString::Reverse(char ch)
@@ -223,31 +198,30 @@ namespace Sibelia
 		return 'N';
 	}
 
-	size_t DnaString::GetBodySize() const
+	size_t DnaString::BytesInBody() const
 	{
 		return capacity_ * sizeof(str_[0]);
 	}
 
 	char DnaString::PopBack()
 	{
-		uint64_t pos;
-		uint64_t element;
-		GetCoord(size_--, element, pos);
-		return str_[element].GetChar(pos);
+		return GetChar(size_--);
 	}
 
 	char DnaString::PopFront()
 	{		
 		size_t remain = size_;
-		char ret = str_[0].GetChar(0);
+		char ret = GetChar(0);
 		for (size_t pos = 0; remain > 0; ++pos)
 		{
-			size_t current = std::min(remain, DnaPiece::CAPACITY);
-			char prev = str_[pos].PopFront();
+			size_t current = std::min(remain, UNIT_CAPACITY);
+			char prev = GetChar(pos, 0);
+			str_[pos] &= ~0x3;
+			str_[pos] >>= 2;
 			remain -= current;
 			if (pos > 0)
 			{				
-				str_[pos - 1].SetChar(DnaPiece::CAPACITY - 1, prev);
+				SetChar(pos - 1, UNIT_CAPACITY - 1, prev);
 			}			
 		}
 
@@ -257,10 +231,7 @@ namespace Sibelia
 
 	void DnaString::AppendBack(char ch)
 	{
-		uint64_t pos;
-		uint64_t element;
-		GetCoord(size_++, element, pos);
-		str_[element].SetChar(pos, ch);
+		SetChar(size_++, ch);
 	}
 
 	void DnaString::AppendFront(char ch)
@@ -268,9 +239,10 @@ namespace Sibelia
 		size_t remain = ++size_;
 		for (size_t pos = 0; remain > 0; ++pos)
 		{			
-			size_t current = std::min(remain, DnaPiece::CAPACITY);
-			char nextCh = str_[pos].GetChar(current - 1);
-			str_[pos].AppendFront(ch);
+			size_t current = std::min(remain, UNIT_CAPACITY);
+			char nextCh = GetChar(pos,  current - 1);
+			str_[pos] <<= 2;
+			str_[pos] |= DnaString::MakeUp(ch);
 			ch = nextCh;
 			remain -= current;
 		}
@@ -278,7 +250,7 @@ namespace Sibelia
 
 	DnaString DnaString::RevComp() const
 	{
-		DnaString ret(size_);
+		DnaString ret(size_, size_);
 		for (size_t i = 0; i < size_; i++)
 		{
 			ret.SetChar(size_ - i - 1, Reverse(GetChar(i)));
@@ -293,7 +265,7 @@ namespace Sibelia
 		{
 			for (size_t i = 0; i < a.capacity_; i++)
 			{
-				if (a.str_[i].GetBody() != b.str_[i].GetBody())
+				if (a.str_[i] != b.str_[i])
 				{
 					return false;
 				}
@@ -321,5 +293,5 @@ namespace Sibelia
 		}
 
 		return false;
-	}
+	}	
 }
--- graphconstructor/src/dnastring.h
+++ graphconstructor/src/dnastring.h
@@ -6,63 +6,43 @@
 
 namespace Sibelia
 {
-	class DnaString;
-	class DnaPiece
-	{
-	public:		
-		char PopFront();
-		void AppendFront(char ch);
-		DnaPiece();		
-		DnaPiece(uint64_t size, uint64_t body);
-		DnaPiece(std::string::const_iterator begin, std::string::const_iterator end);
-		char GetChar(uint64_t idx) const;
-		void SetChar(uint64_t idx, char ch);
-		static uint64_t Prefix(uint64_t body, uint64_t size);
-		size_t GetSize() const;
-		uint64_t GetBody() const;
-		std::string ToString(size_t size) const;
-		static const std::string LITERAL;
-		static const size_t CAPACITY = 32;
-	private:
-		uint64_t body_;
-		static uint64_t MakeUp(char ch);
-	};
-
-	bool operator == (const DnaPiece & a, const DnaPiece & b);
-	bool operator != (const DnaPiece & a, const DnaPiece & b);
-
 	class DnaString
 	{
 	public:
+		void Clear();
 		char PopBack();
-		char PopFront();		
+		char PopFront();	
 		void AppendBack(char ch);
 		void AppendFront(char ch);
 		size_t GetSize() const;
 		size_t MaxSize() const;
-		uint64_t* GetBody() const;
-		size_t GetBodySize() const;
+		size_t BytesInBody() const;
+		uint64_t* GetBody() const;		
 		void Assign(const uint64_t * str);
 		char GetChar(uint64_t idx) const;
 		void SetChar(uint64_t idx, char ch);
 		~DnaString();		
 		DnaString(DnaString && str);
-		DnaString(const DnaString & str);		
-		DnaString(const std::string & str);		
+		DnaString(const DnaString & str);	
+		DnaString(const std::string & str);
+		DnaString(size_t maxSize, size_t size);
 		DnaString(size_t size, const uint64_t * body);
-		DnaString(size_t size, bool capacity = false);
-		
-		DnaString RevComp() const;
+				
 		static char Reverse(char ch);
 		std::string ToString() const;
-		static size_t CalculateCapacity(size_t size);
+		static uint64_t MakeUp(char ch);		
+		DnaString RevComp() const;
 		static std::string RevComp(const std::string & str);
 		static const std::string LITERAL;
 	private:		
 		size_t size_;
 		size_t capacity_;
-		DnaPiece * str_;
-		void GetCoord(uint64_t idx, uint64_t & element, uint64_t & pos) const;
+		uint64_t * str_;		
+		static const size_t UNIT_CAPACITY = 32;		
+		uint64_t TranslateIdx(uint64_t & idx) const;
+		static size_t CalculateCapacity(size_t size);
+		char GetChar(uint64_t element, uint64_t idx) const;
+		void SetChar(uint64_t element, uint64_t idx, char ch);
 		friend bool operator < (const DnaString & a, const DnaString & b);
 		friend bool operator == (const DnaString & a, const DnaString & b);
 		friend bool operator != (const DnaString & a, const DnaString & b);
