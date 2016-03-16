9a39ef882f3bbabab346bad34ac73f2e5efc0173
Wenjia <wxz5086@psu.edu>
2016-03-16 13:18:17 -0400

    working version using standrad unordered map

--- src/graphconstructor/bifurcationstorage.h
+++ src/graphconstructor/bifurcationstorage.h
@@ -1,3 +1,6 @@
+// unordered map implementation
+// initialization done, search in need
+
 #ifndef _BIFURCATION_STORAGE_H_
 #define _BIFURCATION_STORAGE_H_
 
@@ -5,7 +8,8 @@
 #include "compressedstring.h"
 #include "ngramhashing/cyclichash.h"
 #include <fstream>
-#include <cmph.h>
+#include <unordered_map>
+#include <string>
 
 namespace Sibelia
 {
@@ -31,9 +35,6 @@ namespace Sibelia
 
 		void Init(std::istream & bifurcationTempRead, uint64_t verticesCount, uint64_t vertexLength, size_t threads)
 		{
-			std::ofstream outfile;
-			outfile.open("temp.txt", std::ios_base::app);
-	
 			uint64_t bitsPower = 0;
 			vertexLength_ = vertexLength;
 			while (verticesCount * 8 >= (uint64_t(1) << bitsPower))
@@ -52,28 +53,11 @@ namespace Sibelia
 
 				bifurcationKey_.push_back(buf);
 			}
-			/*----------------------------------------------------------------*/
+
 			for(size_t j = 0; j < verticesCount; j++)
 			{
-				outfile << bifurcationKey_.at(j).ToString(vertexLength);
-				outfile << "\n";
+				junction_map.insert({{bifurcationKey_.at(j).ToString(vertexLength), j}});
 			}
-			
-			FILE * keys_fd = fopen("temp.txt", "r");
-
-  			if (keys_fd == NULL) 
-  			{
-  			  fprintf(stderr, "File \"temp.txt\" not found\n");
-  			  exit(1);
-  			}
-	
-  			// Source of keys
-  			cmph_io_adapter_t *source = cmph_io_nlfile_adapter(keys_fd);
-  			cmph_config_t *config = cmph_config_new(source);
-  			cmph_config_set_algo(config, CMPH_BDZ);
-  			new_cmph_hash = cmph_new(config);
-  			cmph_config_destroy(config);
-  			/*----------------------------------------------------------------*/
 		}
 
 		uint64_t GetId(std::string::const_iterator pos) const
@@ -84,61 +68,32 @@ namespace Sibelia
 			uint64_t ret = INVALID_VERTEX;
 			bitBuf.CopyFromString(pos, vertexLength_);
 			
-			
-			/*----------------------------------------------------------------*/
-			const char *pos_key = (bitBuf.ToString(vertexLength_)).c_str();
+			string pos_key = bitBuf.ToString(vertexLength_);
 			bitBuf.Clear();
 			bitBuf.CopyFromReverseString(pos, vertexLength_);
-			const char *neg_key = (bitBuf.ToString(vertexLength_)).c_str();
+			string neg_key = bitBuf.ToString(vertexLength_);
 			
-			unsigned int pos_id = cmph_search(new_cmph_hash, pos_key, (cmph_uint32)strlen(pos_key));
-			if(pos_id != 0)
+			std::unordered_map<std::string,size_t>::const_iterator pos_iter = junction_map.find(pos_key);
+			if(pos_iter != junction_map.end())
 			{
 				posFound = true;
-				ret = pos_id;
+				ret = pos_iter->second;
 			}
 			
 			if(!posFound)
 			{
-				unsigned int neg_id = cmph_search(new_cmph_hash, neg_key, (cmph_uint32)strlen(pos_key));
-				if(neg_id != 0)
+				std::unordered_map<std::string,size_t>::const_iterator neg_iter = junction_map.find(neg_key);
+				if(neg_iter != junction_map.end())
 				{
 					negFound = true;
-					ret = neg_id;
+					ret = neg_iter->second;
 				}
 			}
 			
 			if(negFound && !posFound && !DnaChar::IsSelfReverseCompliment(pos, vertexLength_))
 			{
-				ret = ret + bifurcationKey_.size();;
-			}
-			
-			
-			/*----------------------------------------------------------------
-			
-			auto it = std::lower_bound(bifurcationKey_.begin(), bifurcationKey_.end(), bitBuf, DnaString::Less);
-			if (it != bifurcationKey_.end() && *it == bitBuf)
-			{
-				posFound = true;
-				ret = it - bifurcationKey_.begin();
-			}
-
-			if (!posFound)
-			{
-				bitBuf.Clear();
-				bitBuf.CopyFromReverseString(pos, vertexLength_);
-				auto it = std::lower_bound(bifurcationKey_.begin(), bifurcationKey_.end(), bitBuf, DnaString::Less);
-				if (it != bifurcationKey_.end() && *it == bitBuf)
-				{
-					negFound = true;
-					ret = it - bifurcationKey_.begin();
-				}
-			}
-
-			if (negFound && !posFound && !DnaChar::IsSelfReverseCompliment(pos, vertexLength_))
-			{
 				ret += bifurcationKey_.size();
-			}*/
+			}
 
 			return ret;
 		}
@@ -146,7 +101,8 @@ namespace Sibelia
 	private:
 		size_t vertexLength_;
 		std::vector<DnaString> bifurcationKey_;
-		cmph_t *new_cmph_hash;
+		
+		std::unordered_map<std::string,size_t> junction_map;
 
 	};
 }
